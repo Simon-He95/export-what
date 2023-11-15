081ba410deda872d37dbc8f70e400ce86975dbde
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-15 10:39:40 +0800

    chore: support esm export

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import { createCompletionItem, getCurrentFileUrl, getLineText, registerCompletionItemProvider } from '@vscode-use/utils'
+import { createCompletionItem, getCurrentFileUrl, getLineText, getSelection, registerCompletionItemProvider } from '@vscode-use/utils'
 import { useJSONParse } from 'lazy-js-utils'
 import * as vscode from 'vscode'
 import type { ExtensionContext } from 'vscode'
 
+const suffix = ['.js', '.ts']
+const cache = new Map()
+
+const LOCAL_URL_REG = /^(\.|\/|\@\/)/
 export function activate(context: ExtensionContext) {
   const IMPORT_REG = /import\s*(.*)\s*from ["']([^"']*)["']/
   const REQUIRE_REG = /require\(["']([^"']*)["']\)/
-  const suffix = ['.js', '.ts']
-  const cache = new Map()
 
   context.subscriptions.push(vscode.languages.registerHoverProvider('*', {
     async provideHover(_document, position) {
       const lineText = getLineText(position.line)
       if (!IMPORT_REG.test(lineText) && !REQUIRE_REG.test(lineText))
         return
+      const workspace = vscode.workspace.workspaceFolders![0].uri.path
+      const node_modules = path.resolve(workspace, '.', 'node_modules')
+
       const importMatch = lineText.match(IMPORT_REG)
       if (importMatch) {
         let dep = importMatch[2]
@@ -24,9 +29,8 @@ export function activate(context: ExtensionContext) {
           if (!url)
             return
           const content = await fs.promises.readFile(url, 'utf-8')
-          return getHoverMd(getContentExport(content))
+          return getHoverMd(getContentExport(content, url))
         }
-        const workspace = vscode.workspace.workspaceFolders![0].uri.path
         if (/^(\.|\/|\@\/)/.test(dep)) {
           // relative
           if (dep.startsWith('@')) {
@@ -75,37 +79,17 @@ export function activate(context: ExtensionContext) {
 
           const currentFile = getCurrentFileUrl()
           const url = path.resolve(currentFile, '..', dep)
-          for (const end of suffix) {
-            let temp = url
-            if (!url.endsWith(end))
-              temp += end
-
-            if (!fs.existsSync(temp))
-              continue
-            const content = await fs.promises.readFile(temp, 'utf-8')
-            cache.set(importMatch[2], temp)
-            return getHoverMd(getContentExport(content))
-          }
-          if (await isDirectory(url)) {
-            const folderUrl = `${url}/index`
-            for (const end of suffix) {
-              let temp = folderUrl
-              if (!url.endsWith(end))
-                temp += end
-
-              if (!fs.existsSync(temp))
-                continue
-              const content = await fs.promises.readFile(temp, 'utf-8')
-              cache.set(importMatch[2], temp)
-              return getHoverMd(getContentExport(content))
-            }
+          const target = findFile(url)
+          if (target) {
+            const content = await fs.promises.readFile(target, 'utf-8')
+            cache.set(importMatch[2], target)
+            return getHoverMd(getContentExport(content, target))
           }
 
           cache.set(importMatch[2], null)
         }
         else {
           // node_modules
-          const node_modules = path.resolve(workspace, '.', 'node_modules')
           const moduleFolder = path.resolve(node_modules, '.', dep)
           if (moduleFolder) {
             const url = path.resolve(moduleFolder, '.', 'package.json')
@@ -115,14 +99,15 @@ export function activate(context: ExtensionContext) {
               const url = path.resolve(moduleFolder, '.', main)
               const content = await fs.promises.readFile(url, 'utf-8')
               cache.set(importMatch[2], url)
-              return getHoverMd(getContentExport(content))
+
+              return getHoverMd(getContentExport(content, url))
             }
           }
           cache.set(importMatch[2], null)
         }
       }
       else {
-        // todo
+        // todo: not plan support require
         const requireMatch = lineText.match(REQUIRE_REG)!
         const dep = requireMatch[1]
         if (/^[\.\/]/.test(dep)) {
@@ -137,8 +122,7 @@ export function activate(context: ExtensionContext) {
     },
   }))
 
-  const filter = ['javascript', 'javascriptreact', 'typescriptreact', 'vue', 'svelte']
-
+  const filter = ['javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'svelte']
   context.subscriptions.push(registerCompletionItemProvider(filter, async (_document, position) => {
     const lineText = getLineText(position.line)
     if (!IMPORT_REG.test(lineText) && !REQUIRE_REG.test(lineText))
@@ -152,10 +136,10 @@ export function activate(context: ExtensionContext) {
         if (!url)
           return
         const content = await fs.promises.readFile(url, 'utf-8')
-        return getCompletion(getContentExport(content), importMatch[1])
+        return getCompletion(getContentExport(content, url), importMatch[1])
       }
       const workspace = vscode.workspace.workspaceFolders![0].uri.path
-      if (/^(\.|\/|\@\/)/.test(dep)) {
+      if (LOCAL_URL_REG.test(dep)) {
         // relative
         if (dep.startsWith('@')) {
           const url = path.resolve(workspace, 'jsconfig.json')
@@ -203,32 +187,12 @@ export function activate(context: ExtensionContext) {
 
         const currentFile = getCurrentFileUrl()
         const url = path.resolve(currentFile, '..', dep)
-        for (const end of suffix) {
-          let temp = url
-          if (!url.endsWith(end))
-            temp += end
-
-          if (!fs.existsSync(temp))
-            continue
-          const content = await fs.promises.readFile(temp, 'utf-8')
-          cache.set(importMatch[2], temp)
-          return getCompletion(getContentExport(content), importMatch[1])
+        const target = findFile(url)
+        if (target) {
+          const content = await fs.promises.readFile(target, 'utf-8')
+          cache.set(importMatch[2], target)
+          return getCompletion(getContentExport(content, target), importMatch[1])
         }
-        if (await isDirectory(url)) {
-          const folderUrl = `${url}/index`
-          for (const end of suffix) {
-            let temp = folderUrl
-            if (!url.endsWith(end))
-              temp += end
-
-            if (!fs.existsSync(temp))
-              continue
-            const content = await fs.promises.readFile(temp, 'utf-8')
-            cache.set(importMatch[2], temp)
-            return getCompletion(getContentExport(content), importMatch[1])
-          }
-        }
-
         cache.set(importMatch[2], null)
       }
       else {
@@ -243,7 +207,7 @@ export function activate(context: ExtensionContext) {
             const url = path.resolve(moduleFolder, '.', main)
             const content = await fs.promises.readFile(url, 'utf-8')
             cache.set(importMatch[2], url)
-            return getCompletion(getContentExport(content), importMatch[1])
+            return getCompletion(getContentExport(content, url), importMatch[1])
           }
         }
         cache.set(importMatch[2], null)
@@ -253,7 +217,7 @@ export function activate(context: ExtensionContext) {
       // todo
       const requireMatch = lineText.match(REQUIRE_REG)!
       const dep = requireMatch[1]
-      if (/^[\.\/]/.test(dep)) {
+      if (LOCAL_URL_REG.test(dep)) {
         // relative
         const currentFile = getCurrentFileUrl()
         const url = path.resolve(currentFile, '..', dep)
@@ -271,13 +235,14 @@ export function activate(context: ExtensionContext) {
     md.isTrusted = true
     md.supportHtml = true
     const blocks = []
-    if (exports.length) {
-      blocks.push('## Export')
-      blocks.push(...exports.map(i => `- ${i}`))
-    }
     if (export_default.length) {
       blocks.push('## Export Default')
       blocks.push(...export_default.map(i => `- ${i}`))
+    }
+
+    if (exports.length) {
+      blocks.push('## Export')
+      blocks.push(...exports.map(i => `- ${i}`))
     }
 
     md.appendMarkdown(blocks.join('\n\n'))
@@ -288,11 +253,59 @@ export function activate(context: ExtensionContext) {
   function getCompletion(exportData: { export_default: string[]; exports: string[] }, currentModule: string) {
     const { export_default, exports } = exportData
     const match = currentModule.match(/{([^}]*)}/)
-    const has = match ? match[1].replace(/\s/g, '').split(',').filter(Boolean) : []
+    let has: string[] = []
+    if (match) {
+      has = match[1].replace(/\s/g, '').split(',').filter(Boolean)
+      const _default = match.input?.replace(match[0], '').replace(/[,\s]/g, '')
+      if (_default)
+        has.push(_default)
+    }
 
+    const { character, lineText } = getSelection()!
+    let set_exports_snippet = (v: string) => ` ${v}$1`
+    let show_default = true
+    if (match) {
+      const start = lineText.indexOf(currentModule) + match.index! + match.indexOf(match[1])
+      const end = start + match[0].length - 1
+      if ((character < start) || (character > end)) {
+        // 说明在 {}外
+      }
+      else {
+        show_default = false
+        let pos = character - 1
+        const start = lineText.indexOf(currentModule) + match.index! + match[0].indexOf(match[1])
+        while (lineText[pos] === ' ' && pos > start)
+          pos--
+
+        if (lineText[pos] !== ',')
+          set_exports_snippet = (v: string) => `, ${v}$1`
+        else if (pos !== character - 1)
+          set_exports_snippet = (v: string) => `${v}$1`
+      }
+    }
+    else {
+      if (!currentModule.trim()) {
+        set_exports_snippet = (v: string) => `{ ${v}$1 }`
+      }
+      else {
+        let pos = character - 1
+        const start = lineText.indexOf(currentModule)
+        while (lineText[pos] === ' ' && pos > start)
+          pos--
+
+        if (lineText[pos] !== ',')
+          set_exports_snippet = (v: string) => `, { ${v}$1 }`
+        else if (pos !== character - 1)
+          set_exports_snippet = (v: string) => `{ ${v}$1 }`
+        else
+          set_exports_snippet = (v: string) => ` { ${v}$1 }`
+      }
+    }
     return [
-      ...exports.filter(item => !has.includes(item)).map(item => createCompletionItem({ content: `Export: ${item}`, snippet: item })),
-      ...export_default.map(item => createCompletionItem({ content: `Export Default: ${item}`, snippet: item })),
+      ...exports.filter(item => !has.includes(item)).map(item => createCompletionItem({ content: `Export: ${item}`, snippet: set_exports_snippet(item), type: 8 })),
+      ...show_default
+        ? export_default.filter(item => !has.includes(item)).map(item => createCompletionItem({ content: `Export Default: ${item}`, snippet: item, type: 5 }))
+        : [],
     ]
   }
 }
@@ -305,9 +318,9 @@ const EXPORT_REG = /export (?:const|let|var|function)\s+([\w_]+)\s*/g
 const EXPORT_MULTIPLE_REG = /export\s+{([^}]*)}/g
 const EXPORT_DEFAULT_FUNCTION_REG = /export\s+default\s+function\s+([\w_]+)\s*\([^\)]*\)/g
 const EXPORT_DEFAULT_REG = /export\s+default\s+([\w_]+)[;\s]?$/g
-// 暂时不考虑复杂的*的dep递归追踪module
+const TREE_MODULE_REG = /export\s+\*\s+from\s+["']([^'"]*)["']/g
 
-export function getContentExport(content: string) {
+export function getContentExport(content: string, workspace: string): { export_default: string[]; exports: string[] } {
   const exports = []
   const export_default = []
   for (const match of content.matchAll(EXPORT_REG))
@@ -317,12 +330,69 @@ export function getContentExport(content: string) {
     export_default.push(match[1])
 
   for (const match of content.matchAll(EXPORT_MULTIPLE_REG)) {
-    const items = match[1].replace(/\s/g, '').split(',')
-    exports.push(...items.map(i => i.split(':')[0]))
+    const items = match[1].trim().split(',')
+    exports.push(...items.map((i) => {
+      i = i.trim().replace(/\s+/g, ' ')
+      const asMatch = i.match(/ as\s+(.*)/)
+      if (asMatch) {
+        const asValue = asMatch[1].trim()
+        if (asValue === 'default') {
+          export_default.push(asValue)
+          return false
+        }
+        else { return asValue }
+      }
+      return i.split(':')[0]
+    }).filter(Boolean) as string[])
   }
 
   for (const match of content.matchAll(EXPORT_DEFAULT_REG))
     export_default.push(match[1])
+
+  for (const match of content.matchAll(TREE_MODULE_REG)) {
+    const tree_url = match[1]
+    if (cache.has(tree_url)) {
+      const url = cache.get(tree_url)
+      if (!url)
+        continue
+      const tree_content = fs.readFileSync(url, 'utf-8')
+      const { exports: treeExports } = getContentExport(tree_content, url)
+      exports.push(...treeExports)
+      continue
+    }
+    // 判断是否是node_modules or 相对路径
+    if (LOCAL_URL_REG.test(tree_url)) {
+      const url = path.resolve(workspace, '..', tree_url)
+      const target = findFile(url)
+      if (target) {
+        const tree_content = fs.readFileSync(target, 'utf-8')
+        const { exports: treeExports } = getContentExport(tree_content, target)
+        exports.push(...treeExports)
+      }
+      else {
+        cache.set(tree_url, null)
+      }
+    }
+    else {
+      const node_modules = path.resolve(vscode.workspace.workspaceFolders![0].uri.path, '.', 'node_modules')
+      const moduleFolder = path.resolve(node_modules, '.', tree_url)
+      if (moduleFolder) {
+        const url = path.resolve(moduleFolder, '.', 'package.json')
+        const pkg = JSON.parse(fs.readFileSync(url, 'utf-8'))
+        const main = pkg.module || pkg.main
+        if (main) {
+          const url = path.resolve(moduleFolder, '.', main)
+          const tree_content = fs.readFileSync(url, 'utf-8')
+          cache.set(tree_url, url)
+          const { exports: treeExports } = getContentExport(tree_content, url)
+          exports.push(...treeExports)
+        }
+        else {
+          cache.set(tree_url, null)
+        }
+      }
+    }
+  }
 
   return {
     export_default,
@@ -330,15 +400,26 @@ export function getContentExport(content: string) {
   }
 }
 
-export function isDirectory(url: string): Promise<boolean> {
-  return new Promise((resolve, _reject) => {
-    fs.stat(url, (err, stats) => {
-      if (err) {
-        resolve(false)
-        return
-      }
+export function isDirectory(url: string) {
+  const stats = fs.statSync(url)
+  return stats.isDirectory()
+}
 
-      resolve(stats.isDirectory())
-    })
-  })
+export function findFile(url: string) {
+  const target = null
+  for (const s of suffix) {
+    if (url.endsWith(s))
+      return url
+
+    const temp = `${url}${s}`
+    if (fs.existsSync(temp))
+      return temp
+  }
+  if (target)
+    return target
+
+  if (!isDirectory(url))
+    return target
+
+  return findFile(`${url}/index`)
 }
